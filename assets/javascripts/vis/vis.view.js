ba9c97de4b3f34417a29a63eb89055bf4fb4957e
David McClure <davidwilliammcclure@gmail.com>
2014-08-06 10:32:32 -0700

    Moving in current code.

--- assets/javascripts/vis/vis.view.js
+++ assets/javascripts/vis/vis.view.js
@@ -12,38 +12,228 @@ Neatline.module('Vis', function(Vis) {
   Vis.View = Backbone.View.extend({
 
 
+    events: {
+      'click': 'maximize'
+    },
+
+
+    // STARTUP
+    // ------------------------------------------------------------------------
+
+
     /**
-     * Initialize the timeline.
+     * Initialize the collection and timeline.
      */
-    initialize: function() {
+    initialize: function(options) {
 
-      var groups = new vis.DataSet([
-        {id: 1, content: 'Group 1'},
-        {id: 2, content: 'Group 2'},
-        {id: 3, content: 'Group 3'},
-      ]);
+      this.slug = options.slug;
 
-      var items = new vis.DataSet([
-        {id: 1, group: 1, content: 'Item 1', start: '2014-07-01'},
-        {id: 2, group: 1, content: 'Item 2', start: '2014-07-02'},
-        {id: 3, group: 1, content: 'Item 3', start: '2014-07-03'},
-        {id: 4, group: 1, content: 'Item 4', start: '2014-07-04'},
-        {id: 5, group: 1, content: 'Item 5', start: '2014-07-05'},
-        {id: 6, group: 2, content: 'Item 6', start: '2014-07-06'},
-        {id: 7, group: 2, content: 'Item 7', start: '2014-07-07'},
-        {id: 8, group: 2, content: 'Item 8', start: '2014-07-08'},
-        {id: 9, group: 2, content: 'Item 9', start: '2014-07-09'},
-        {id: 10, group: 2, content: 'Item 10', start: '2014-07-10'},
-        {id: 11, group: 3, content: 'Item 11', start: '2014-07-11'},
-        {id: 12, group: 3, content: 'Item 12', start: '2014-07-12'},
-        {id: 13, group: 3, content: 'Item 13', start: '2014-07-13'},
-        {id: 14, group: 3, content: 'Item 14', start: '2014-07-14'},
-        {id: 15, group: 3, content: 'Item 15', start: '2014-07-15'},
-      ]);
+      // Spin up an empty records collection.
+      this.records = new Neatline.Shared.Record.Collection();
 
-      this.timeline = new vis.Timeline(this.el, items);
+      // Create timeline.
+      this._initTimeline();
+      this._initHighlight();
+      this._initSelect();
+      this._initMaximize();
+
+    },
+
+
+    /**
+     * Initialize the timeline and groups.
+     */
+    _initTimeline: function() {
+
+      // Register the bands.
+      var groups = new vis.DataSet();
+      _.each(Vis.bands, function(band) {
+        groups.add({ id: band.tag, content: band.title });
+      });
+
+      // Spin up the timeline.
+      this.timeline = new vis.Timeline(this.el);
       this.timeline.setGroups(groups);
 
+      // Set display options.
+      //this.timeline.setOptions({
+        //padding: 2
+      //});
+
+    },
+
+
+    /**
+     * Listen for event highlights.
+     */
+    _initHighlight: function() {
+      // TODO: Need to implement this in Vis.js.
+    },
+
+
+    /**
+     * Listen for event selections.
+     */
+    _initSelect: function() {
+
+      this.selected = [] // The currently-selected ids.
+
+      // When an event is selected.
+      this.timeline.on('select', _.bind(function(args) {
+
+        // Which IDs were added and removed?
+        var a = _.difference(args.items, this.selected);
+        var r = _.difference(this.selected, args.items);
+
+        // Select the added IDs, unselect the removed.
+        _.each(a, _.bind(this.publishSelect, this));
+        _.each(r, _.bind(this.publishUnselect, this));
+
+        // Set the new IDs.
+        this.selected = args.items;
+
+      }, this));
+
+    },
+
+
+    /**
+     * Maximize when the zoom level is changed.
+     */
+    _initMaximize: function() {
+      this.timeline.on('rangechanged', _.bind(this.maximize, this));
+    },
+
+
+    // RECORDS
+    // ------------------------------------------------------------------------
+
+
+    /**
+     * Load records for the timeline.
+     */
+    load: function() {
+      var params = { hasDate: true };
+      this.records.update(params, _.bind(this.ingest, this));
+    },
+
+
+    /**
+     * Clear the timeline and render a new collection.
+     *
+     * @param {Object} records
+     */
+    ingest: function(records) {
+
+      var events = new vis.DataSet();
+
+      records.each(function(record) {
+
+        // Pass if no start date.
+        var start = record.get('start_date');
+        if (!start) return;
+
+        // Default parameters.
+        var event = {
+          id:       record.id,
+          content:  record.get('title'),
+          start:    start,
+          model:    record
+        };
+
+        // If defined, add end date.
+        var end = record.get('end_date');
+        if (end) event['end'] = end;
+
+        // Set the group.
+        _.each(Vis.bands, function(band) {
+          if (record.hasTag(band.tag)) {
+            event['group'] = band.tag;
+          }
+        });
+
+        events.add(event);
+
+      });
+
+      // Render the collection.
+      this.timeline.setItems(events);
+
+    },
+
+
+    // EVENTS
+    // ------------------------------------------------------------------------
+
+
+    /**
+     * Publish a record selection.
+     *
+     * @param {Number} id
+     */
+    publishSelect: function(id) {
+      this.publish('select', this.records.get(id));
+    },
+
+
+    /**
+     * Publish a record unselection.
+     *
+     * @param {Number} id
+     */
+    publishUnselect: function(id) {
+      this.publish('unselect', this.records.get(id));
+    },
+
+
+    /**
+     * Render a record selection.
+     *
+     * @param {Object} model
+     */
+    renderSelect: function(model) {
+      this.selected = [model.id]
+      this.timeline.setSelection(this.selected);
+    },
+
+
+    /**
+     * Remove a record selection.
+     *
+     * @param {Object} model
+     */
+    renderUnselect: function(model) {
+      this.selected = _.without(this.selected, model.id);
+      this.timeline.setSelection(this.selected);
+    },
+
+
+    /**
+     * Shrink the timeline.
+     */
+    minimize: function() {
+      this.$el.addClass('minimized');
+    },
+
+
+    /**
+     * Expand the timeline.
+     */
+    maximize: function() {
+      this.$el.removeClass('minimized');
+    },
+
+
+    /**
+     * Broadcast a public event.
+     *
+     * @param {String} event
+     * @param {Object} model
+     */
+    publish: function(event, model) {
+      Neatline.vent.trigger(event, {
+        model: model, source: this.slug
+      });
     }
 
 
