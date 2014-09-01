8e725fd5fd9a8e286f60ce031d65f2d8c424c609
David McClure <davidwilliammcclure@gmail.com>
2014-09-01 11:38:04 -0700

    Committing show.php, adds map resize script.

---
+++ show.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * @package     omeka
+ * @subpackage  whitney-detail
+ * @copyright   2014 David McClure
+ * @license     http://www.apache.org/licenses/LICENSE-2.0.html
+ */
+
+?>
+
+<?php echo head(array(
+  'title' => nl_getExhibitField('title'),
+  'bodyclass' => 'neatline show'
+)); ?>
+
+<!-- Exhibit. -->
+<div class="exhibit">
+  <?php echo nl_getExhibitMarkup(); ?>
+</div>
+
+<?php echo foot(); ?>
