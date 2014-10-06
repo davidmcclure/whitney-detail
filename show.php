b040d9f831120098616cadac510724ee54b0fc16
David McClure <davidwilliammcclure@gmail.com>
2014-10-06 16:27:35 -0700

    Adds tagline.

--- show.php
+++ show.php
@@ -23,6 +23,7 @@
 <div id="branding">
   <h1 class="brand">Anne Whitney Abroad, 1867-1868</h1>
   <h1 class="title">The First Sixteen Months</h1>
+  <p class="tagline">A companion to Jacqueline Marie Musacchio, "<a href="http://www.19thc-artworldwide.org/index.php/autumn14/musacchio-introduction" target="_blank">Mapping the 'White, Marmorean Flock': Anne Whitney Abroad, 1867-1868</a>," Nineteenth-Century Art Worldwide 13, no. 2 (2014).</p>
 </div>
 
 <!-- Zoom. -->
